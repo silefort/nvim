78e90af96737728ba7b305da774011f48191dcd2
Simon Lefort <simon.lefort@gmail.com>
2023-05-15 07:18:43 +0200

    plugin(nvim-tmux-navigator): setup nvim-tmux-navigator plugin and associated bindings

--- lua/silefort/packer.lua
+++ lua/silefort/packer.lua
@@ -29,6 +29,21 @@ return require('packer').startup(function(use)
 
   use ('tpope/vim-fugitive')
 
+  use { 'alexghergh/nvim-tmux-navigation', config = function()
+
+        local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+        nvim_tmux_nav.setup {
+            disable_when_zoomed = true -- defaults to false
+        }
+
+        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    end
+  }
+
   use {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
