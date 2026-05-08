d08b5887ac2eb7b55db7fac004a0b660936698d3
simon.lefort <simon.lefort@octo.com>
2026-05-08 18:10:38 +0000

    feat(commands): ajouter Buffer reload pour recharger le buffer depuis le disque

--- lua/commands/buffer.lua
+++ lua/commands/buffer.lua
@@ -7,3 +7,7 @@ end, { desc = "Sauvegarder le buffer courant" })
 cmd.register("buffer", "close", function()
   vim.cmd("bdelete")
 end, { desc = "Fermer le buffer courant" })
+
+cmd.register("buffer", "reload", function()
+  vim.cmd("edit!")
+end, { desc = "Recharger le buffer courant depuis le disque" })
